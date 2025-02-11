d74ed81235afce534e5d7c1960a0d412237ac575
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2025-02-11 13:10:32 -0500

    add ssm session manager plugin installation

--- .local/bin/provision.sh
+++ .local/bin/provision.sh
@@ -318,6 +318,18 @@ if [ -n "$SSH_KEY_NAME_GITHUB" ]; then
   fi
 fi
 
+# add aws ssm sessions plugin
+if [[ ! -f "/usr/local/bin/session-manager-plugin" ]]; then
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+    unzip sessionmanager-bundle.zip
+    sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+  else
+    curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
+    sudo dpkg -i session-manager-plugin.deb
+  fi
+fi
+
 # add to docker group if linux
 if [[ "$OSTYPE" != "darwin"* && ! $(groups $USER) =~ \bdocker\b ]]; then
   sudo groupadd docker
