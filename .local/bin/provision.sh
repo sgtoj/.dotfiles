a38cafd77119d66ec733e80e63459043f515322f
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2025-02-11 13:12:30 -0500

    clean up after session manager plugin install

--- .local/bin/provision.sh
+++ .local/bin/provision.sh
@@ -328,6 +328,7 @@ if [[ ! -f "/usr/local/bin/session-manager-plugin" ]]; then
     curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/ubuntu_64bit/session-manager-plugin.deb" -o "session-manager-plugin.deb"
     sudo dpkg -i session-manager-plugin.deb
   fi
+  rm -rf sessionmanager-bundle*
 fi
 
 # add to docker group if linux
