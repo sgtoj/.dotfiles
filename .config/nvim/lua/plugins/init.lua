86d68fc829ef5101757b1be35c76e6cd972fcb27
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2025-12-18 10:01:40 -0500

    add copilot.vim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -71,4 +71,15 @@ return {
       smear_insert_mode = true,
     },
   },
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false,
+      })
+    end,
+  },
 }
