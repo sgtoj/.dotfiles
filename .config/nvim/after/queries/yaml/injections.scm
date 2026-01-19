e6fc0cfda9171d96ad1aee9413294024cad7ce98
Brian Ojeda <9335829+sgtoj@users.noreply.github.com>
2026-01-19 17:13:03 -0500

    fix syntax hightlighting

--- .config/nvim/after/queries/yaml/injections.scm
+++ .config/nvim/after/queries/yaml/injections.scm
@@ -1,32 +1,25 @@
 ; extends
 
-; Inject bash for block scalars preceded by "# language=bash" comment
-; Works with Atmos commands, and is portable across editors (JetBrains, etc.)
-;
-; Example:
-;   steps:
-;     # language=bash
-;     - |
-;       set -e
-;       echo "hello"
+; Key-based injection for "steps" (like built-in "run", "script")
+(block_mapping_pair
+  key: (flow_node) @_run
+  (#any-of? @_run "steps")
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (block_node
+          (block_scalar) @injection.content
+          (#set! injection.language "bash")
+          (#offset! @injection.content 0 1 0 0))))))
 
-(block_sequence
-  (comment) @_lang
-  .
-  (block_sequence_item
-    (block_node
-      (block_scalar) @injection.content))
-  (#match? @_lang "language=bash")
-  (#set! injection.language "bash")
-  (#offset! @injection.content 0 1 0 0))
-
-; Also handle plain scalar (single-line) after comment
-(block_sequence
-  (comment) @_lang
-  .
-  (block_sequence_item
-    (flow_node
-      (plain_scalar
-        (string_scalar) @injection.content)))
-  (#match? @_lang "language=bash")
-  (#set! injection.language "bash"))
+; Comment-based injection: # language=bash before block scalar
+(block_mapping_pair
+  key: (flow_node)
+  (comment) @_lang (#match? @_lang "language=bash")
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (block_node
+          (block_scalar) @injection.content
+          (#set! injection.language "bash")
+          (#offset! @injection.content 0 1 0 0))))))
